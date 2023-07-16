c488940b96f969cea806df6a8b693e1bc48439eb
Mykyta Shcheholevatyi <tosniki91@gmail.com>
2023-07-16 15:40:02 +0200

    Add files via upload

--- Chat-Observer-Extension/function observeChat.js
+++ Chat-Observer-Extension/function observeChat.js
@@ -43,19 +43,3 @@ document.addEventListener("DOMContentLoaded", function() {
 
   observeChat();
 });
-
-document.getElementById("sendRandomButton").addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    let randomNumber = Math.floor(Math.random() * 10000000000).toString().padStart(10, "0");
-    chrome.tabs.executeScript(tabs[0].id, {
-      code: `
-        var inputField = document.querySelector(".im_editable");
-        if (inputField) {
-          inputField.innerHTML = "${randomNumber}";
-          var event = new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter', keyCode: 13, which: 13, bubbles: true });
-          inputField.dispatchEvent(event);
-        }
-      `,
-    });
-  });
-});
